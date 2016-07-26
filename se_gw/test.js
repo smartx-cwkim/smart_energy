0c3fe89b5035ae0d303d8663ba71472df169e145
Jeongju Bae <bjj@smartx.kr>
2016-07-26 18:56:52 +0900

    100

--- se_gw/test.js
+++ se_gw/test.js
@@ -41,7 +41,7 @@ var controller = function(delays, total, cb){
 var tController = timely.async(controller);
 
 var dataExtractor = function(){
-  factorial(200);
+  factorial(100);
   return 0;
 };
 var tDataExtractor = timely(dataExtractor);
