6929e0fb5aaad11037ef60d83767118cdb99b922
Jeongju Bae <bjj@smartx.kr>
2016-07-28 16:36:21 +0900

    add end monitoring

--- se_end/server.js
+++ se_end/server.js
@@ -2,10 +2,20 @@ var express = require('express');
 var request = require('sync-request');
 var responseTime = require('response-time');
 var os = require('os');
+var influx = require('influx');
+
+var influxClient = influx({
+  host : 'K-BOX',
+  port : 8086, // optional, default 8086
+  protocol : 'http', // optional, default 'http'
+  username : 'admin',
+  password : 'admin',
+  database : 'pvis2'
+});
 
 var app = express();
 var period = 100;
-var density = 1;
+var density = 10;
 
 var kafka = require('kafka-node'),
     Producer = kafka.Producer,
@@ -31,6 +41,7 @@ app.use(responseTime());
 
 producer.on('ready', function(){
   var sendData = function(){
+    var start = new Date();
     env = {"temp": 24+Math.random()*1, "humidity": 0.4+Math.random()*0.05};
     energy = {"volt": volt, "mampere": 0.5+Math.random()*0.05};
     data = {"ip": addresss[0], "when": new Date(), "where":"c208", "env":env, "energy":energy};
@@ -38,6 +49,9 @@ producer.on('ready', function(){
     payloads = [{topic:'se', messages: sdata}];
     producer.send(payloads, function (err, d) {
       console.log(data);
+      var end = new Date();
+      var diff = end-start;
+      influxClient.writePoint('pvis2', {time: new Date(), delay: diff}, null, function(err, res){console.log(err);});
     });
     setTimeout(sendData, period);
   }
